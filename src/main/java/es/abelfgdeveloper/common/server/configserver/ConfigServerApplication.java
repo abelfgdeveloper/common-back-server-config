888007dfa49f390b851797f3ff8ff7643053040c
Abel <abeldevelop@users.noreply.github.com>
2021-03-07 22:25:31 +0100

    Create the project

---
+++ src/main/java/es/abelfgdeveloper/common/server/configserver/ConfigServerApplication.java
@@ -0,0 +1,12 @@
+package es.abelfgdeveloper.common.server.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
+}
---
+++ src/test/java/es/abelfgdeveloper/common/server/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package es.abelfgdeveloper.common.server.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
