a778e4702dbe3ab76b06a8a4a1bb2b974a6cd05e
Abel <abeldevelop@users.noreply.github.com>
2021-03-08 19:39:25 +0100

    Fix the config server

--- src/main/java/es/abelfgdeveloper/common/server/config/ConfigServerApplication.java
+++ src/main/java/es/abelfgdeveloper/common/server/config/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package es.abelfgdeveloper.common.server.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
